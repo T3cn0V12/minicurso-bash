2949b7159027cef89a6d4aceba673191abcbcb76
T3cn0V12 <ifar3086@gmail.com>
2026-07-05 14:06:37 -0400

    EjerciciosDeScripts

---
+++ ejecicios/hoy.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "La fecha de hoy es:"
+date
+echo "El directorio actual es:"
+pwd
+echo "Mas graficamente:"
+tree -h
---
+++ ejecicios/operacionesread.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+read -p "Ingrese un numero para sumar:" a
+read -p "Ingrese otro numero para sumar" b
+
+echo "La suma entre $a y $b es: $((a+b))"
